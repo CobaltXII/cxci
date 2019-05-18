eea5cc33f25635dd26226580bfdbd0764c4c1fbc
CobaltXII <sidatt64@gmail.com>
2019-05-18 14:09:56 -0400

    Added list of ANSI escape codes for all implemented ANSI colors

--- ansi_colors.hpp
+++ ansi_colors.hpp
@@ -22,3 +22,24 @@ enum ansi_color_t {
 	bold_white,
 	reset
 };
+
+// All available color escape codes.
+std::string ansi_color_code[] = {
+	"\x1b[30m",
+	"\x1b[31m",
+	"\x1b[32m",
+	"\x1b[33m",
+	"\x1b[34m",
+	"\x1b[35m",
+	"\x1b[36m",
+	"\x1b[37m",
+	"\x1b[30;1m",
+	"\x1b[31;1m",
+	"\x1b[32;1m",
+	"\x1b[33;1m",
+	"\x1b[34;1m",
+	"\x1b[35;1m",
+	"\x1b[36;1m",
+	"\x1b[37;1m",
+	"\x1b[0m"
+};
