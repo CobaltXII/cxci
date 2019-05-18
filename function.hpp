ee8fa4b1f5258d979262c2a31c75cbb937192aaa
CobaltXII <sidatt64@gmail.com>
2019-05-18 14:18:28 -0400

    Added logical function class

---
+++ function.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <vector>
+
+// A function.
+struct function_t {
+	type_t						type;
+	identifier_t				identifier;
+	std::vector<parameter_t>	parameters;
+	std::vector<statement_t*>	body;
+};
