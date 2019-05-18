827092e2e661d435584388985a6cfdd908beccaa
CobaltXII <sidatt64@gmail.com>
2019-05-18 14:20:56 -0400

    Create parser_debug.c

---
+++ c/parser_debug.c
@@ -0,0 +1,17 @@
+// // Returns the length of a string.
+// size_t strlen(char* str) {
+// 	size_t len = 0;
+// 	while (*(str++) != 0) {
+// 		len++;
+// 	}
+// 	return len;
+// }
+
+// Returns the length of a string.
+int strlen(int* str) {
+	int len = [];
+	while (*str != 0) {
+		str = str + 1;
+		len = len + 1;
+	}
+}
