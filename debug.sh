71ad847416a01eae5aa0b683cb97f889fc60175f
CobaltXII <sidatt64@gmail.com>
2019-05-18 15:58:10 -0400

    Renamed parser_* to *

---
+++ debug.sh
@@ -0,0 +1,1 @@
+clang++ debug.cpp -o debug.o -std=c++11 -Wall && ./debug.o c/debug.c
--- parser_debug.sh
+++
@@ -1,1 +0,0 @@
-clang++ parser_debug.cpp -o parser_debug.o -std=c++11 -Wall && ./parser_debug.o c/parser_debug.c
