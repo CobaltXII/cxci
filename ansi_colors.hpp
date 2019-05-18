ed6d5ff8e184dc4801b64f52c2631834212808e1
CobaltXII <sidatt64@gmail.com>
2019-05-18 14:09:36 -0400

    Added enumeration of all implemented ANSI colors

---
+++ ansi_colors.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+// All available colors.
+enum ansi_color_t {
+	black,
+	red,
+	green,
+	yellow,
+	blue,
+	magenta,
+	cyan,
+	white,
+	bold_black,
+	bold_red,
+	bold_green,
+	bold_yellow,
+	bold_blue,
+	bold_magenta,
+	bold_cyan,
+	bold_white,
+	reset
+};
